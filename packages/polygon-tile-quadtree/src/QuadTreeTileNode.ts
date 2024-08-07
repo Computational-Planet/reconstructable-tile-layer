d7bd44d817a3c6083f2276ddff3d6c02fc819771
RogerWQH2023 <rogerzju2021@gmail.com>
2024-08-07 17:59:08 +0800

    feat: 增加对双根节点的情况的支持

--- packages/polygon-tile-quadtree/src/QuadTreeTileNode.ts
+++ packages/polygon-tile-quadtree/src/QuadTreeTileNode.ts
@@ -1,5 +1,5 @@
 import { Rectangle } from "cesium";
-import { clipPolygonByQuadTreeNodes } from "./utils/geometry";
+import { checkClipMode, clipPolygonByQuadTreeNodes } from "./utils/geometry";
 
 export enum TileClipMode {
   NONE_DISPLAY,
@@ -22,90 +22,142 @@ export interface NodeChild {
 
 export interface NodeInfo {
   tileXYL: TileXYL;
-  polygon: Array<number>;
+  polygon: Array<number> | null;
 }
 
 export class QuadTreeTileNode {
   private _rectangle: Rectangle;
   private _tileXYL: TileXYL;
-  private _polygon: Array<number>;
+  private _polygon: Array<number> | null = null;
   private _status: TileClipMode;
   private _child: NodeChild | null = null;
 
+  // 设置两种构造函数。如果不提供多边形则说明完全显示，不需要裁剪
+  constructor(x: number, y: number, l: number, rec: Rectangle);
   constructor(
     x: number,
     y: number,
     l: number,
     rec: Rectangle,
     polygon: Array<number>
+  );
+  constructor(
+    x: number,
+    y: number,
+    l: number,
+    rec: Rectangle,
+    polygon?: Array<number>
   ) {
     this._tileXYL = { x: x, y: y, l: l };
     this._rectangle = rec;
-    this._polygon = polygon;
-    if (polygon.length === 0) {
-      this._status = TileClipMode.NONE_DISPLAY;
-      this._child = null;
+    if (!polygon) {
+      this._status = TileClipMode.FULL_DISPLAY;
     } else {
-      this._status = TileClipMode.NEED_CLIP;
+      this._polygon = polygon;
+      this._status = checkClipMode(polygon);
+      if (this._status === TileClipMode.NONE_DISPLAY) {
+        // 如果整个瓦片不显示，则不需要再记录polygon，将子节点置为null（之后将没有子节点了）
+        this._polygon = null;
+        this._child = null;
+      } else if (this._status === TileClipMode.FULL_DISPLAY) {
+        // 如果整个瓦片都显示，则之后不需要再记录polygon，直接显示整个瓦片。
+        this._polygon = null;
+      }
     }
   }
 
   // 分裂子节点
-  splitNode() {
-    // 如果当前状态健康，且还没有生成子节点，则创建子节点
-    if (this._status !== TileClipMode.NONE_DISPLAY && !this._child) {
-      // 获得裁剪出的子节点多边形
-      const { polygonLB, polygonLT, polygonRB, polygonRT } =
-        clipPolygonByQuadTreeNodes(this._polygon);
-      const x0 = this._tileXYL.x;
-      const y0 = this._tileXYL.y;
-      const l0 = this._tileXYL.l;
-      // 获得矩形四至，以及中心点位置
-      const west0 = this._rectangle.west;
-      const east0 = this._rectangle.east;
-      const north0 = this._rectangle.north;
-      const south0 = this._rectangle.south;
-      const centerWE = (west0 + east0) / 2;
-      const centerNS = (south0 + north0) / 2;
+  splitNodeIfNeeded() {
+    if (this._child || this._status === TileClipMode.NONE_DISPLAY) {
+      return; //如果瓦片已经生成，或者整个瓦片不显示，则直接退出
+    }
+    // 获得瓦片xyl
+    const x0 = this._tileXYL.x;
+    const y0 = this._tileXYL.y;
+    const l0 = this._tileXYL.l;
+    // 获得矩形四至，以及中心点位置
+    const west0 = this._rectangle.west;
+    const east0 = this._rectangle.east;
+    const north0 = this._rectangle.north;
+    const south0 = this._rectangle.south;
+    const centerWE = (west0 + east0) / 2;
+    const centerNS = (south0 + north0) / 2;
+    if (this._status === TileClipMode.FULL_DISPLAY) {
+      //如果当前瓦片已经完全显示，则按简化方法拓展节点
       // 创建子节点
       this._child = {
         lb: new QuadTreeTileNode(
           2 * x0,
           2 * y0 + 1,
           l0 + 1,
-          new Rectangle(west0, south0, centerWE, centerNS),
-          polygonLB
+          new Rectangle(west0, south0, centerWE, centerNS)
         ),
         lt: new QuadTreeTileNode(
           2 * x0,
           2 * y0,
           l0 + 1,
-          new Rectangle(west0, centerNS, centerWE, north0),
-          polygonLT
+          new Rectangle(west0, centerNS, centerWE, north0)
         ),
         rb: new QuadTreeTileNode(
           2 * x0 + 1,
           2 * y0 + 1,
           l0 + 1,
-          new Rectangle(centerWE, south0, east0, centerNS),
-          polygonRB
+          new Rectangle(centerWE, south0, east0, centerNS)
         ),
         rt: new QuadTreeTileNode(
           2 * x0 + 1,
           2 * y0,
           l0 + 1,
-          new Rectangle(centerWE, centerNS, east0, north0),
-          polygonRT
+          new Rectangle(centerWE, centerNS, east0, north0)
         ),
       };
-    } else this._child = null; //如果整个瓦片都不显示，则不生成子节点
+      return;
+    }
+
+    // 在接下来的情况下，瓦片状态一定为待裁剪，且此时多边形一定存在。
+
+    // 获得裁剪出的子节点多边形
+    const { polygonLB, polygonLT, polygonRB, polygonRT } =
+      clipPolygonByQuadTreeNodes(this._polygon!);
+
+    // 创建子节点
+    this._child = {
+      lb: new QuadTreeTileNode(
+        2 * x0,
+        2 * y0 + 1,
+        l0 + 1,
+        new Rectangle(west0, south0, centerWE, centerNS),
+        polygonLB
+      ),
+      lt: new QuadTreeTileNode(
+        2 * x0,
+        2 * y0,
+        l0 + 1,
+        new Rectangle(west0, centerNS, centerWE, north0),
+        polygonLT
+      ),
+      rb: new QuadTreeTileNode(
+        2 * x0 + 1,
+        2 * y0 + 1,
+        l0 + 1,
+        new Rectangle(centerWE, south0, east0, centerNS),
+        polygonRB
+      ),
+      rt: new QuadTreeTileNode(
+        2 * x0 + 1,
+        2 * y0,
+        l0 + 1,
+        new Rectangle(centerWE, centerNS, east0, north0),
+        polygonRT
+      ),
+    };
   }
 
   // 递归拓展树，直到找到所有指定层级的子节点
   getTileInfoByLevel(level: number, result: Array<NodeInfo>) {
     if (this._tileXYL.l < level) {
       // 如果当前四叉树等级还不到要求的等级，则继续分裂并搜索节点
-      this.splitNode(); // 注意：是否需要分裂节点由分裂节点函数自己考虑，不需要在这里判断。
+      this.splitNodeIfNeeded(); // 注意：是否需要分裂节点由分裂节点函数自己考虑，不需要在这里判断。
       if (this._child) {
         this._child.lb.getTileInfoByLevel(level, result);
         this._child.lt.getTileInfoByLevel(level, result);
@@ -116,6 +168,7 @@ export class QuadTreeTileNode {
       // 当前层级正确时，如果当前瓦片并非完全不可见，则将瓦片信息推入结果
       if (this._status !== TileClipMode.NONE_DISPLAY) {
         result.push({ tileXYL: this._tileXYL, polygon: this._polygon });
+        // 注意：这里返回的多边形可能是null，当返回值为null时，则意思是当前瓦片完全可见，不需要裁剪。
       }
       return;
     }
--- packages/polygon-tile-quadtree/src/QuadTreeTileProcesser.ts
+++ packages/polygon-tile-quadtree/src/QuadTreeTileProcesser.ts
@@ -2,6 +2,7 @@ import { ImageryProvider, Rectangle } from "cesium";
 import { NodeInfo, QuadTreeTileNode, TileXYL } from "./QuadTreeTileNode";
 import { CesiumTileProcesser } from "tile-processer-webgl";
 import { PI_10 } from "./constants";
+import { clipToLR } from "./utils/geometry";
 
 export class QuadTreeTileProcesser {
   private _imageryProvider: ImageryProvider;
@@ -36,13 +37,33 @@ export class QuadTreeTileProcesser {
     this._imageryProvider = provider;
     this._tileProcesser = processer;
     this._rootXYL = rootXYL;
+    const rectangleL = provider.tilingScheme.tileXYToRectangle(
+      rootXYL.x,
+      rootXYL.y,
+      rootXYL.l
+    );
     this._rectangle = provider.tilingScheme.tileXYToRectangle(
       rootXYL.x,
       rootXYL.y,
       rootXYL.l
     );
-    // 注，之后还需要考虑多边形的分割问题（如果是分左右的节点则需要分割后再赋值）
-    //先将多边形处理为[0,1]
+    if (rootRXYL) {
+      // rootNum>2的情况还在施工中
+      this._rootNum = 2;
+      this._rootRXYL = rootRXYL;
+      const rectangleR = provider.tilingScheme.tileXYToRectangle(
+        rootRXYL.x,
+        rootRXYL.y,
+        rootRXYL.l
+      );
+      // 最后合并两个矩形范围，作为最终的四叉树多边形范围
+      this._rectangle = Rectangle.union(rectangleL, rectangleR);
+      if (!this._rectangle) {
+        throw new Error("未能获得瓦片四叉树矩形区域!");
+      }
+    }
+
+    // 先将多边形处理为[0,1]
     const west = this._rectangle.west;
     const east = this._rectangle.east;
     const north = this._rectangle.north;
@@ -58,41 +79,63 @@ export class QuadTreeTileProcesser {
     }
     clipPolygon[clipPolygon.length - 2] = clipPolygon[0];
     clipPolygon[clipPolygon.length - 1] = clipPolygon[1];
-    //然后构造根节点
-    this._root = new QuadTreeTileNode(
-      rootXYL.x,
-      rootXYL.y,
-      rootXYL.l,
-      this._rectangle.clone(), // 这里需要克隆，因为Rectangle之后可能还会变化的
-      clipPolygon
-    );
 
-    // rootNum>2的情况还在施工中
-    if (rootRXYL !== undefined) {
-      this._rootNum = 2;
-      this._rootRXYL == rootRXYL;
-      const RectangleR = provider.tilingScheme.tileXYToRectangle(
+    if (!rootRXYL) {
+      //然后构造根节点
+      this._root = new QuadTreeTileNode(
+        rootXYL.x,
+        rootXYL.y,
+        rootXYL.l,
+        rectangleL, // 这里需要克隆，因为这个Rectangle之后可能会变化的
+        clipPolygon
+      );
+    } else {
+      const rectangleR = provider.tilingScheme.tileXYToRectangle(
         rootRXYL.x,
         rootRXYL.y,
         rootRXYL.l
       );
-
-      this._rectangle = Rectangle.simpleIntersection(
-        this._rectangle,
-        RectangleR
-      );
-      if (!this._rectangle) {
-        throw new Error("未能获得瓦片四叉树矩形区域!");
+      // 将多边形进行裁剪（要求：瓦片四叉树范围一定完全包含多边形）
+      const { polygonL, polygonR } = clipToLR(clipPolygon, 0.5);
+      // 将左右根节点的坐标恢复到[0,1]的状态
+      for (let i = 0; i < polygonL.length; i = i + 2) {
+        polygonL[i] = polygonL[i] * 2;
       }
+      for (let i = 0; i < polygonR.length; i = i + 2) {
+        polygonR[i] = (polygonR[i] - 0.5) * 2;
+      }
+
+      // 此时再创建两个根节点
+      //然后构造根节点
+      this._root = new QuadTreeTileNode(
+        rootXYL.x,
+        rootXYL.y,
+        rootXYL.l,
+        this._rectangle.clone(), // 这里需要克隆，因为这个Rectangle之后可能会变化的
+        polygonL
+      );
+      //然后构造右边的根节点
+      this._rootR = new QuadTreeTileNode(
+        rootRXYL.x,
+        rootRXYL.y,
+        rootRXYL.l,
+        rectangleR.clone(), // 这里需要克隆，因为这个Rectangle之后可能会变化的
+        polygonR
+      );
     }
   }
 
   findTilesByLevel(level: number, result: Array<NodeInfo>) {
     if (level < this._rootXYL!.l) {
+      console.log("level<");
       return [];
     }
-    if (this._root) this._root.getTileInfoByLevel(level, result);
-    if (this._rootR) this._rootR.getTileInfoByLevel(level, result);
+    if (this._root) {
+      this._root.getTileInfoByLevel(level, result);
+    }
+    if (this._rootR) {
+      this._rootR.getTileInfoByLevel(level, result);
+    }
     return result;
   }
 
@@ -105,10 +148,16 @@ export class QuadTreeTileProcesser {
   get rootNum() {
     return this._rootNum;
   }
+  get rootXYL() {
+    return this._rootXYL;
+  }
   get root() {
     return this._root;
   }
   get rootR() {
     return this._rootR;
+  }
+  get rootRXYL() {
+    return this._rootRXYL;
   }
 }
