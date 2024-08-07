1e8c7c7348481da29bd9fb0e241787f91d8ce1ed
RogerWQH2023 <rogerzju2021@gmail.com>
2024-08-07 17:58:28 +0800

    feat: 实现对瓦片完全、部分、不显示状态进行判断的函数

--- packages/polygon-tile-quadtree/src/utils/geometry.ts
+++ packages/polygon-tile-quadtree/src/utils/geometry.ts
@@ -4,6 +4,7 @@
 // 因此，初始瓦片范围也是[0,1]
 
 import { DEFAULT_ACCURATE } from "src/constants";
+import { TileClipMode } from "src/QuadTreeTileNode";
 
 // 在计算完成后需要遍历所有的子多边形，将他们的坐标恢复到[0, 1]的状态。
 export function clipPolygonByQuadTreeNodes(polygon: Array<number>) {
@@ -219,4 +220,115 @@ export function clipToBT(polygon: Array<number>, yi: number) {
     polygonT.push(polygonT[1]);
   }
   return { polygonB, polygonT };
+}
+
+// 判断节点多边形是否占满裁剪窗口的算法：
+// 可以应对有重叠边的情况
+//将遍历顶点的步骤划分为四个状态
+// 这里用数字标识状态方便计算
+function checkPointState(x: number, y: number) {
+  if (y === 0 && x < 1) return 0; // 对应(0,0)到(1,0),不包括(1,0)
+  else if (x === 1 && y < 1) return 1; // 对应(1,0)到(1,1),不包括(1,1)
+  else if (y === 1 && x > 0) return 2; // 对应(1,1)到(0,1),不包括(0,1)
+  else if (x === 0 && y > 0) return 3; // 对应(0,1)到(0,0),不包括(0,0)
+  else return null; // 不在边界上（即肯定需要裁剪）
+}
+
+// 切换状态时如果当前点不在当前状态的起点，则说明有裁剪，直接停止
+export function checkClipMode(polygon: Array<number>) {
+  if (polygon.length < 6) return TileClipMode.NONE_DISPLAY; // 至少三点才能形成多边形，如果少于3个点则无法形成多边形，设置为完全不显示。
+  let oriState = checkPointState(polygon[0], polygon[1]);
+  if (oriState === null) return TileClipMode.NEED_CLIP;
+  let lastState = oriState;
+  // 我们需要判断，初始状态出发后，最终又回到初始状态时是否是从同一边回来的
+  // 所以记录首次触发的下一个状态，以及最后回到原状态前最后的状态
+  let oriOut: number = -1;
+  let lastIn: number = -1;
+
+  for (let i = 2; i < polygon.length; i = i + 2) {
+    // 记录当前顶点的x，y坐标
+    const x = polygon[i];
+    const y = polygon[i + 1];
+    let currentState = checkPointState(x, y);
+
+    if (currentState === null) return TileClipMode.NEED_CLIP; // 如果当前点不在多边形边界，则说明多边形需要裁剪
+    if (currentState === lastState) continue; // 如果状态未变化，则直接进入下一个循环
+    if (currentState !== lastState) {
+      if (
+        currentState !== (lastState + 4 + 1) % 4 &&
+        currentState !== (lastState + 4 - 1) % 4
+      ) {
+        /* console.log(
+          `${lastState},${currentState};${(lastState + 4 + 1) % 4},${
+            (lastState + 4 - 1) % 4
+          }`
+        );
+        console.log(polygon); */
+        return TileClipMode.NEED_CLIP; // 如果状态变化不是按顺序进行的，则需要裁剪(跳过一个状态则会出现裁剪边)
+      }
+
+      // 计算各种情况下的合法点
+      // 获取上个点
+      const x0 = polygon[i - 2];
+      const y0 = polygon[i - 1];
+      let needClip = true;
+      switch (currentState) {
+        case 0:
+          if (lastState === 3) {
+            // 3———>0，正向，用当前点判断
+            if (x === 0 && y === 0) needClip = false;
+          } else {
+            // 1———>0，反向，用上个点判断
+            if (x0 === 1 && y0 === 0) needClip = false;
+          }
+          break;
+        case 1:
+          if (lastState === 0) {
+            // 0———>1，正向，用当前点判断
+            if (x === 1 && y === 0) needClip = false;
+          } else {
+            // 2———>1，反向，用上个点判断
+            if (x0 === 1 && y0 === 1) needClip = false;
+          }
+          break;
+        case 2:
+          if (lastState === 1) {
+            // 1———>2，正向，用当前点判断
+            if (x === 1 && y === 1) needClip = false;
+          } else {
+            // 3———>2，反向，用上个点判断
+            if (x0 === 0 && y0 === 1) needClip = false;
+          }
+          break;
+        case 3:
+          if (lastState === 2) {
+            // 2———>3，正向，用当前点判断
+            if (x === 0 && y === 1) needClip = false;
+          } else {
+            // 0———>3，反向，用上个点判断
+            if (x0 === 0 && y0 === 0) needClip = false;
+          }
+          break;
+        default:
+      }
+      if (needClip) {
+        //console.log(polygon);
+        return TileClipMode.NEED_CLIP; // 如果该边并非到矩形顶点后转折，则需要裁剪
+      }
+      // 经过了上面的判断，之后的情况一定是：点发生了合法的边的转换。
+      // 所以我们需要记录一下是否发生了关于初始状态的状态变化
+      if (oriOut === -1 && lastState === oriState) {
+        oriOut = currentState; // 记录首次离开初始状态的值
+      }
+      if (currentState === oriState) {
+        lastIn = lastState; // 不断更新最后回到初始状态前的状态值
+      }
+      // 都完成以后，将当前状态赋值给上个状态，进入下一轮循环
+      lastState = currentState;
+    }
+  }
+  // 如果最终活着走出了这个循环，则说明这个多边形要么就是完整的正方形，要么就是面积为0的边的残留。
+  if (oriOut === lastIn) {
+    return TileClipMode.NONE_DISPLAY; //如果出入一致，说明没包裹成形状，全空
+  } else return TileClipMode.FULL_DISPLAY; // 如果不一致，说明进行了一个完整的轮回，全满。
 }
